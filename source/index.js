6192c4979104df0293fedac06eb06f0841811fe8
Katerina <comina.ru@gmail.com>
2022-05-10 16:03:50 +0300

    feat: add ru keyboard

--- source/index.js
+++ source/index.js
@@ -1,6 +1,5 @@
 import './css/style.css';
 import keyboardArr from './keyboard.json';
-console.log(keyboardArr);
 
 const wrapper = document.querySelector('#wrapper');
 
@@ -22,22 +21,35 @@ description.classList.add('description');
 description.innerText = 'Клавиатура создана в операционной системе Windows';
 wrapper.appendChild(description);
 
+const languageText = document.createElement('p');
+languageText.classList.add('language');
+languageText.innerText = 'Для переключения языка комбинация: левыe Ctrl + Alt';
+wrapper.appendChild(languageText);
+
 let capsLock = false;
+let language = '';
+language = localStorage.getItem('language') ? localStorage.getItem('language') : 'En';
 
 let keys;
 let keysEn;
 let keysShiftEn;
 let keysCapsEn;
 let keysShiftCapsEn;
+let keysRu;
+let keysShiftRu;
+let keysCapsRu;
+let keysShiftCapsRu;
 
 let currentKey;
 let textareaContent = '';
 let visibleChild;
+let pressed = new Set();
+let codes = ['ControlLeft', 'AltLeft'];
 
 function initKeyboard() {
   let out = '';
   for(let i = 0; i < keyboardArr.length; i++) {
-    out += `<div class="keyboard__item ${keyboardArr[i].code}" data-code="${keyboardArr[i].code}"><span class="key key--normal">${keyboardArr[i].key}</span><span class="key key--shift">${keyboardArr[i].keyShift}</span><span class="key key--caps">${keyboardArr[i].keyCaps}</span><span class="key key--shift-caps">${keyboardArr[i].keyShiftCaps}</span></div>`;
+    out += `<div class="keyboard__item ${keyboardArr[i].code}" data-code="${keyboardArr[i].code}"><span class="key key--normal">${keyboardArr[i].key}</span><span class="key key--shift">${keyboardArr[i].keyShift}</span><span class="key key--caps">${keyboardArr[i].keyCaps}</span><span class="key key--shift-caps">${keyboardArr[i].keyShiftCaps}</span><span class="key key--normal-ru">${keyboardArr[i].keyRu}</span><span class="key key--shift-ru">${keyboardArr[i].keyShiftRu}</span><span class="key key--caps-ru">${keyboardArr[i].keyCapsRu}</span><span class="key key--shift-caps-ru">${keyboardArr[i].keyShiftCapsRu}</span></div>`;
   }
   keyboard.innerHTML = out;
 
@@ -46,6 +58,10 @@ function initKeyboard() {
   keysShiftEn = document.querySelectorAll('.key--shift');
   keysCapsEn = document.querySelectorAll('.key--caps');
   keysShiftCapsEn = document.querySelectorAll('.key--shift-caps');
+  keysRu = document.querySelectorAll('.key--normal-ru');
+  keysShiftRu = document.querySelectorAll('.key--shift-ru');
+  keysCapsRu = document.querySelectorAll('.key--caps-ru');
+  keysShiftCapsRu = document.querySelectorAll('.key--shift-caps-ru');
 }
 initKeyboard();
 
@@ -57,14 +73,14 @@ function addVisible(activeKey) {
     el.classList.add('visible');
   });
 }
-addVisible(keysEn);
+language === 'En' ? addVisible(keysEn) : addVisible(keysRu);
 
 let selStart = 0;
 let selEnd = 0;
 
-textarea.addEventListener("blur", function() {
-  selStart = this.selectionStart;
-  selEnd = this.selectionEnd;
+textarea.addEventListener("blur", () => {
+  selStart = textarea.selectionStart;
+  selEnd = textarea.selectionEnd;
 });
 
 function addText(text) {
@@ -86,11 +102,11 @@ function addAnswer(code) {
       if (capsLock) {
         capsLock = false;
         currentKey.classList.remove('active');
-        addVisible(keysEn);
+        language === 'En' ? addVisible(keysEn) : addVisible(keysRu);
       } else {
         capsLock = true;
         currentKey.classList.add('active');
-        addVisible(keysCapsEn);
+        language === 'En' ? addVisible(keysCapsEn) : addVisible(keysCapsRu);
       }
       break
 
@@ -115,9 +131,9 @@ function addAnswer(code) {
     case 'ShiftLeft':
     case 'ShiftRight':
       if (capsLock) {
-        addVisible(keysShiftCapsEn);
+        language === 'En' ? addVisible(keysShiftCapsEn) : addVisible(keysShiftCapsRu);
       } else {
-        addVisible(keysShiftEn);
+        language === 'En' ? addVisible(keysShiftEn) : addVisible(keysShiftRu);
       }
       break
 
@@ -139,28 +155,43 @@ function addAnswer(code) {
   textarea.selectionEnd = selEnd;
 }
 
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', (event) => {
   event.preventDefault();
 
   currentKey = document.querySelector(`.keyboard__item.${event.code}`);
   currentKey.classList.add('active');
   visibleChild = currentKey.querySelector('.visible');
   addAnswer(event.code);
+
+  pressed.add(event.code);
+  for (let code of codes) {
+    if (!pressed.has(code)) {
+      return;
+    }
+  }
+  language = language === 'En' ? 'Ru' : 'En';
+  localStorage.setItem('language', language);
+  if (capsLock) {
+    language === 'En' ? addVisible(keysCapsEn) : addVisible(keysCapsRu);
+  } else {
+    language === 'En' ? addVisible(keysEn) : addVisible(keysRu);
+  }
 })
 
-document.addEventListener('keyup', function(event) {
+document.addEventListener('keyup', (event) => {
   document.querySelectorAll('.keyboard__item').forEach(function(el){
     if (!el.classList.contains('CapsLock') && !el.classList.contains('ShiftLeft') && !el.classList.contains('ShiftRight')) {
       el.classList.remove('active');
     }
     if (event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
       if (capsLock) {
-        addVisible(keysCapsEn);
+        language === 'En' ? addVisible(keysCapsEn) : addVisible(keysCapsRu);
       } else {
-        addVisible(keysEn);
+        language === 'En' ? addVisible(keysEn) : addVisible(keysRu);
       }
       el.classList.remove('active');
     }
+    pressed.delete(event.code);
   });
 })
 
@@ -182,9 +213,9 @@ document.querySelectorAll('.keyboard__item').forEach(function(el){
 document.querySelectorAll('.ShiftLeft, .ShiftRight').forEach(function(el){
   el.addEventListener('mousedown', function(){
     if (capsLock) {
-      addVisible(keysShiftCapsEn);
+      language === 'En' ? addVisible(keysShiftCapsEn) : addVisible(keysShiftCapsRu);
     } else {
-      addVisible(keysShiftEn);
+      language === 'En' ? addVisible(keysShiftEn) : addVisible(keysShiftRu);
     }
   })
 });
@@ -192,9 +223,9 @@ document.querySelectorAll('.ShiftLeft, .ShiftRight').forEach(function(el){
 document.querySelectorAll('.ShiftLeft, .ShiftRight').forEach(function(el){
   el.addEventListener('mouseup', function(){
     if (capsLock) {
-      addVisible(keysCapsEn);
+      language === 'En' ? addVisible(keysCapsEn) : addVisible(keysCapsRu);
     } else {
-      addVisible(keysEn);
+      language === 'En' ? addVisible(keysEn) : addVisible(keysRu);
     }
   })
 });
